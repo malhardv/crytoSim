98c8d25d2311b3a80d8d54340cd5807dcf46c00e
malhardv <malhar.vhatkar@gmail.com>
2025-04-30 22:59:13 +0530

    2

--- server/index.js
+++ server/index.js
@@ -37,6 +37,11 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Server Running")
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
