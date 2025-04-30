636e517c44121f7b42c8d23f7b5da06e3fc2abb4
malhardv <malhar.vhatkar@gmail.com>
2025-05-01 01:21:16 +0530

    added the cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://cryto-sim.vercel.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
